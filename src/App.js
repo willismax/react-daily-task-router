5608a969dfc45f6bea4e7097ae1adbc18fcc6181
unknown <willismax.com@gmail.com>
2022-08-19 21:08:37 +0800

    day15

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-import './App.css';
+import "./App.css";
 import {
-  HashRouter,
+  BrowserRouter,
+  Route,
+  Routes,
   NavLink,
-  Rotues,
-  Rotue,
-  useNavigate
-} from 'react-router-dom';
+  useNavigate,
+  Outlet,
+  useParams,
+} from "react-router-dom";
 
 const Todo = () => {
-let navigate = useNagivate();
-  return 
-  <>
-    <p>這是 Todo 頁面 </p>
-    <button onClick={()=>{
-    navigate('/login')
-    )}回到登入</button>
-  </>;
+  return (
+    <>
+      <p>Todo</p>
+      <LogOut />
+    </>
+  );
 };
+
 const Login = () => {
   return <p>這是登入頁面</p>;
 };
@@ -24,10 +25,34 @@ const Register = () => {
   return <p>這是註冊頁面</p>;
 };
 
+const LogOut = () => {
+  const navigate = useNavigate();
+  return (
+    <button
+      onClick={() => {
+        navigate("/login");
+      }}>登出</button>
+  );
+};
+
+const Post = () => {
+  return (
+    <div>
+      <h3>Post 頁面</h3>
+      <Outlet />
+    </div>
+  );
+};
+
+const PostId = () => {
+  let params = useParams();
+  return <p>Post: {params.postId}</p>;
+};
+
 function App() {
   return (
     <div className="container">
-      <HashRouter>
+      <BrowserRouter>
         <div className="nav-link">
           <NavLink to="/">
             <p>回到首頁</p>
@@ -41,16 +66,24 @@ function App() {
           <NavLink to="/todo">
             <p>Todo 頁面</p>
           </NavLink>
+          <NavLink to="/post">
+            <p>Post 頁面</p>
+          </NavLink>
+          <NavLink to="/post/HelloWorld">
+            <p>Post 詳細頁面</p>
+          </NavLink>
         </div>
-        <Routes>
-          <Route path="/" element={<p>這是首頁</p>} />
-          <Route path="register" element={<Register />} />
-          <Route path="login" element={<Login />} />
-          <Route path="todo" element={<Todo />} />
-        </Routes>
         {/* Routes, Route 練習區 */}
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/Register" element={<Register />} />
+          <Route path="/Todo" element={<Todo />} />
+          <Route path="/post" element={<Post />}>
+            <Route path=":postId" element={<PostId />} />
+          </Route>
+        </Routes>
         {/* 練習區 */}
-      </HashRouter>
+      </BrowserRouter>
     </div>
   );
 }
